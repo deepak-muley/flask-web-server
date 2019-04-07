3032aa582da52474d9e128a054f5128b37176029
Deepak Muley <deepak.muley@gmail.com>
2019-04-07 14:35:38 -0700

    added create_application api

--- xi_iot_sdk.py
+++ xi_iot_sdk.py
@@ -9,17 +9,7 @@ pp = pprint.PrettyPrinter()
 class ResourceNotFound(Exception):
     
     def __init__(self, message):
-        self.message = message
-
-class Xi:
-    resources = {}
-
-    @classmethod
-    def Resource(cls, resource_name):
-        cls.resources['xi_iot'] = XiIoT()
-        if resource_name in cls.resources:
-            return cls.resources[resource_name]
-        raise ResourceNotFound("Invalid resource name provided: %s" % resource_name) 
+        self.message = message 
 
 class XiIoT:
     def __init__(self):
@@ -40,7 +30,6 @@ class XiIoT:
         self.connection.request("GET", "/v1.0/projects", headers=headers)
         resp = self.connection.getresponse()
         resp = json.loads(resp.read())
-        pp.pprint(resp)
         projects = []
         for projectDict in resp['result']:
             projects.append(Project(projectDict))
@@ -51,7 +40,6 @@ class XiIoT:
         self.connection.request("GET", "/v1.0/applications", headers=headers)
         resp = self.connection.getresponse()
         resp = json.loads(resp.read())
-        pp.pprint(resp)
         apps = []
         for appDict in resp['result']:
             apps.append(Application(appDict))
@@ -61,9 +49,30 @@ class Project:
     def __init__(self, adict):
         self.__dict__.update(adict)
 
+    def create_application(self, appObject):
+        xiIot = Xi.Resource('xi_iot')
+        payload = json.dumps(appObject.__dict__)
+        headers = { 'authorization': "Bearer %s" % xiIot.token }
+        xiIot.connection.request("POST", "/v1.0/applications", payload, headers)
+        resp = xiIot.connection.getresponse()
+        resp = json.loads(resp.read())
+        pp.pprint(resp)
+        return resp['id']
+
 class Application:
     def __init__(self, adict):
         self.__dict__.update(adict)
+
+class Xi:
+    resources = {}
+    xiIoT = XiIoT()
+
+    @classmethod
+    def Resource(cls, resource_name):
+        cls.resources['xi_iot'] = cls.xiIoT
+        if resource_name in cls.resources:
+            return cls.resources[resource_name]
+        raise ResourceNotFound("Invalid resource name provided: %s" % resource_name)
 
 if __name__ == "__main__":
     xi_iot = Xi.Resource("xi_iot")
@@ -73,13 +82,24 @@ if __name__ == "__main__":
         pp.pprint("Project name: %s" % project.name)
         pp.pprint("--id: %s" % project.id)
         pp.pprint("--edgeIds: %s" % project.edgeIds)
-        pp.pprint("\n")
+        print("\n")
 
     apps = xi_iot.get_applications()
     for app in apps:
         pp.pprint("Application name: %s" % app.name)
         pp.pprint("--id: %s" % app.id)
-        pp.pprint("\n")
+        print("\n")
+
+    project = projects[0]
+    appDict = {}
+    yamlFile = open("./flask-web-server.yaml", "r") 
+    appDict['appManifest'] = yamlFile.read()
+    appDict['name'] = "flask-web-server2"
+    appDict['description'] = "test api created app"
+    appDict['projectId'] = project.id
+    app = Application(appDict)
+    appId = project.create_application(app)
+    pp.pprint("Application with Id %s created." % appId)
 
 
 
