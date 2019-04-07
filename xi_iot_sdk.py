393bbe2ac2da640bb46e08d5034cc1690dba4bad
Deepak Muley <deepak.muley@gmail.com>
2019-04-07 16:46:09 -0700

    added app status apis

--- xi_iot_sdk.py
+++ xi_iot_sdk.py
@@ -29,7 +29,7 @@ class XiIoT:
         resp = json.loads(resp.read())
         self.token = resp['token']
 
-    def get_projects(self):
+    def list_projects(self):
         headers = { 'authorization': "Bearer %s" % self.token }
         self.connection.request("GET", "/v1.0/projects", headers=headers)
         resp = self.connection.getresponse()
@@ -41,7 +41,7 @@ class XiIoT:
             projects.append(Project(projectDict))
         return projects
 
-    def get_applications(self):
+    def list_applications(self):
         headers = { 'authorization': "Bearer %s" % self.token }
         self.connection.request("GET", "/v1.0/applications", headers=headers)
         resp = self.connection.getresponse()
@@ -52,6 +52,18 @@ class XiIoT:
         for appDict in resp['result']:
             apps.append(Application(appDict))
         return apps
+
+    def list_all_application_statuses(self):
+        headers = { 'authorization': "Bearer %s" % self.token }
+        self.connection.request("GET", "/v1.0/applicationstatuses", headers=headers)
+        resp = self.connection.getresponse()
+        if (resp.status != 200):
+            raise APIFailed(resp.status, resp.reason, resp.read())
+        resp = json.loads(resp.read())
+        app_statuses = []
+        for app_status in resp['result']:
+            app_statuses.append(app_status)
+        return app_statuses
 
 class Project:
     def __init__(self, adict):
@@ -97,8 +109,20 @@ class Project:
         if (resp.status != 200):
             raise APIFailed(resp.status, resp.reason, resp.read())
         resp = json.loads(resp.read())
-        pp.pprint(resp)
         return resp['id']
+
+    def list_edges(self):
+        xiIot = Xi.Resource('xi_iot')
+        headers = { 'authorization': "Bearer %s" % xiIot.token }
+        xiIot.connection.request("GET", "/v1.0/projects/%s/edges" % self.id, headers=headers)
+        resp = xiIot.connection.getresponse()
+        if (resp.status != 200):
+            raise APIFailed(resp.status, resp.reason, resp.read())
+        resp = json.loads(resp.read())
+        apps = []
+        for appDict in resp['result']:
+            apps.append(Application(appDict))
+        return apps
 
 class Application:
     def __init__(self, adict):
@@ -118,39 +142,68 @@ class Xi:
 if __name__ == "__main__":
     xi_iot = Xi.Resource("xi_iot")
     xi_iot.login(xi_iot_env_constants.userEmail, xi_iot_env_constants.userPwd)
-    projects = xi_iot.get_projects()
+    projects = xi_iot.list_projects()
     for project in projects:
         pp.pprint("Project name: %s" % project.name)
         pp.pprint("--id: %s" % project.id)
         pp.pprint("--edgeIds: %s" % project.edgeIds)
         print("\n")
 
-    apps = xi_iot.get_applications()
+    apps = xi_iot.list_applications()
     for app in apps:
         pp.pprint("Application name: %s" % app.name)
         pp.pprint("--id: %s" % app.id)
         print("\n")
 
     project = projects[0]
+    edges = project.list_edges()
+    edgeIds = []
+    for edge in edges:
+        pp.pprint("Edge name: %s" % edge.name)
+        pp.pprint("--id: %s" % edge.id)
+        edgeIds.append(edge.id)
+        print("\n")
 
     with open("./flask-web-server.yaml", "r") as yamlFile:
         yamlData = yamlFile.read()
 
     #Create App
     appDict = {}
-     
     appDict['appManifest'] = yamlData
     appDict['name'] = "flask-web-server"
     appDict['description'] = "test api created app"
     appDict['projectId'] = project.id
+    appDict['edgeIds'] = edgeIds
     app = Application(appDict)
     appId = project.create_application(app)
     pp.pprint("Application with Id %s created." % appId)
+    print("\n")
+
+    app2Dict = {}
+    app2Dict['appManifest'] = yamlData
+    app2Dict['name'] = "flask-web-server2"
+    app2Dict['description'] = "test api created app"
+    app2Dict['projectId'] = project.id
+    app2Dict['edgeIds'] = edgeIds
+    app2 = Application(app2Dict)
+    app2Id = project.create_application(app2)
+    pp.pprint("Application with Id %s created." % app2Id)
+    print("\n")
 
     #Get App
     app = project.get_application(appId)
     pp.pprint("Get Application Info with Id %s" % appId)
     pp.pprint("Application Details: %s" % app.__dict__)
+    print("\n")
+
+    #Get App statuses
+    app_statuses = xi_iot.list_all_application_statuses()
+    while len(app_statuses) == 0:
+        pp.pprint("app status: %s" % app_statuses)
+        app_statuses = xi_iot.list_all_application_statuses()
+    for app_status in app_statuses:
+        pp.pprint("Application status: %s" % app_status)
+        print("\n")
 
     #Update App
     appDict = {}
@@ -158,9 +211,11 @@ if __name__ == "__main__":
     appDict['name'] = "flask-web-server5"
     appDict['description'] = "test api created app 5"
     appDict['projectId'] = project.id
+    appDict['edgeIds'] = edgeIds
     app = Application(appDict)
     appId = project.update_application(appId, app)
     pp.pprint("Application with Id %s updated." % appId)
+    print("\n")
 
     #Delete App
     appDict = {}
@@ -169,5 +224,14 @@ if __name__ == "__main__":
     app = Application(appDict)
     appId = project.delete_application(appId)
     pp.pprint("Application with Id %s deleted." % appId)
+    print("\n")
 
+    #Delete App2
+    app2Dict = {}
+    app2Dict['projectId'] = project.id
+    app2Dict['id'] = app2Id
+    app2 = Application(app2Dict)
+    app2Id = project.delete_application(app2Id)
+    pp.pprint("Application with Id %s deleted." % app2Id)
+    print("\n")
 
