c7af6794a2bd6660831a48909992d3e4a402d958
Deepak Muley <deepak.muley@gmail.com>
2019-04-07 10:43:57 -0700

    added working python xi iot sdk

---
+++ xi_iot_env_constants.py
@@ -0,0 +1,6 @@
+
+
+#Enter your environment specific information
+userEmail = "<enter your user email>"
+userPwd = "<enter your user pwd>"
+xi_iot_api_endpoint = "iot.nutanix.com"
--- xi_iot_get_projects.py
+++ xi_iot_get_projects.py
@@ -2,22 +2,65 @@ import http.client
 import json
 import pprint
 
+import xi_iot_env_constants
+
 pp = pprint.PrettyPrinter()
 
-userEmail = "test@test.com"
-userPwd = "test"
-conn = http.client.HTTPSConnection("iot.nutanix.com")
-payload = '{\"email\":\"%s\",\"password\":\"%s\"}' % (userEmail, userPwd)
-headers = { 'content-type': "application/json" }
-conn.request("POST","/v1.0/login", payload, headers)
-res = conn.getresponse()
-data = json.loads(res.read())
-pp.pprint(data)
-print('\n')
+class ResourceNotFound(Exception):
+    
+    def __init__(self, message):
+        self.message = message
 
-headers = { 'authorization': "Bearer %s" % data["token"] }
-conn.request("GET", "/v1.0/projects", headers=headers)
-res = conn.getresponse()
-data = json.loads(res.read())
-pp.pprint(data)
+class Xi:
+    resources = {}
+
+    @classmethod
+    def Resource(cls, resource_name):
+        cls.resources['xi_iot'] = XiIoT()
+        if resource_name in cls.resources:
+            return cls.resources[resource_name]
+        raise ResourceNotFound("Invalid resource name provided: %s" % resource_name) 
+
+class XiIoT:
+    def __init__(self):
+        self.connection = None
+        self.token = None
+
+    def login(self, userEmail, userPwd):
+        self.connection = http.client.HTTPSConnection(xi_iot_env_constants.xi_iot_api_endpoint)
+        payload = '{\"email\":\"%s\",\"password\":\"%s\"}' % (userEmail, userPwd)
+        headers = { 'content-type': "application/json" }
+        self.connection.request("POST","/v1.0/login", payload, headers)
+        resp = self.connection.getresponse()
+        resp = json.loads(resp.read())
+        self.token = resp['token']
+
+    def get_projects(self):
+        headers = { 'authorization': "Bearer %s" % self.token }
+        self.connection.request("GET", "/v1.0/projects", headers=headers)
+        resp = self.connection.getresponse()
+        resp = json.loads(resp.read())
+        projects = []
+        for projectDict in resp['result']:
+            projects.append(Project(projectDict))
+        return projects
+
+class Project:
+    def __init__(self, adict):
+        self.__dict__.update(adict)
+
+class Application:
+    def __init__(self, adict):
+        self.__dict__.update(adict)
+
+xi_iot = Xi.Resource("xi_iot")
+xi_iot.login(xi_iot_env_constants.userEmail, xi_iot_env_constants.userPwd)
+projects = xi_iot.get_projects()
+for project in projects:
+    pp.pprint("name: %s" % project.name)
+    pp.pprint("  id: %s" % project.id)
+    pp.pprint("  edgeIds: %s" % project.edgeIds)
+
+
+
 
